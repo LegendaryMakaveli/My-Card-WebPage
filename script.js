226b17144abf00bb25c25f74cdaa2d65d905475c
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-01-12 09:18:55 +0100

    Card Web Page

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+   $("#showDiv") .click(function(){
+       $("#myDiv") .css("display","flex")
+   })
+})
+$(document).ready(function() {
+    $("#hideDiv") .click(function(){
+        $("#myDiv") .css("display","none")
+    })
+ })
+ $(document).ready(function() {
+    $("#toggleShow") .click(function(){
+        $("#remsub") .css("display","block")
+    })
+ })
+ $(document).ready(function() {
+    $("#toggleHide") .click(function(){
+        $("#remsub") .css("display","none")
+    })
+ })
+console.log()
